c8d8b84658c73e9c0d7da36a81e2378a7bf36eae
trios4504 <67927315+trios4504@users.noreply.github.com>
2021-01-20 18:36:56 -0700

    Add files via upload

---
+++ while_loops/while_loops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace while_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool runLoop = true;
+
+            //we are printing out all the even numbers
+            // between 0 and 10
+            int counter = 0;
+            while (counter <= 10)
+            {
+                /*
+                if(counter % 2 == 0)
+                {
+                    Console.WriteLine($"{counter} is an even number");
+                }
+                */
+
+                if (counter % 2 == 1)
+                {
+                    Console.WriteLine($"{counter} is an odd number");
+                }
+                counter++;
+            }
+        }
+    }
+}
